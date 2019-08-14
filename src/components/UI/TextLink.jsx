944d1b24e42ed674be61a7629e0e513eda498138
Michael Daniel Padilla <michaeldpadilla@gmail.com>
2019-08-13 21:09:30 -0700

    Text Link no longer using styled components

--- src/components/UI/TextLink.jsx
+++ src/components/UI/TextLink.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { Link as ReachLink } from '@reach/router';
 import { Link } from '@material-ui/core';
-import { styled } from '@material-ui/styles';
-
-const TextStyleLink = styled(Link)({
-  textDecoration: `none`,
-});
 
 const TextLink = props => {
   const { children } = props;
+
   return (
-    <TextStyleLink component={ReachLink} {...props}>
+    <Link underline="none" component={ReachLink} {...props}>
       {children}
-    </TextStyleLink>
+    </Link>
   );
 };
 
